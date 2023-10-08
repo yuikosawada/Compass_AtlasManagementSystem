8cfe94300f6a03f128ed73b1e40d9a1648da3174
YuikoSawada <yuiko.sawada419@gmail.com>
2023-10-08 13:14:49 +0900

    過去の日付に『受付終了』を表示完了

--- app/Calendars/General/CalendarView.php
+++ app/Calendars/General/CalendarView.php
@@ -1,21 +1,26 @@
 <?php
+
 namespace App\Calendars\General;
 
 use Carbon\Carbon;
 use Auth;
 
-class CalendarView{
+class CalendarView
+{
 
   private $carbon;
-  function __construct($date){
+  function __construct($date)
+  {
     $this->carbon = new Carbon($date);
   }
 
-  public function getTitle(){
+  public function getTitle()
+  {
     return $this->carbon->format('Y年n月');
   }
 
-  function render(){
+  function render()
+  {
     $html = [];
     $html[] = '<div class="calendar text-center">';
     $html[] = '<table class="table">';
@@ -32,39 +37,54 @@ class CalendarView{
     $html[] = '</thead>';
     $html[] = '<tbody>';
     $weeks = $this->getWeeks();
-    foreach($weeks as $week){
-      $html[] = '<tr class="'.$week->getClassName().'">';
+    foreach ($weeks as $week) {
+      $html[] = '<tr class="' . $week->getClassName() . '">';
 
       $days = $week->getDays();
-      foreach($days as $day){
+      foreach ($days as $day) {
         $startDay = $this->carbon->copy()->format("Y-m-01");
         $toDay = $this->carbon->copy()->format("Y-m-d");
 
-        if($startDay <= $day->everyDay() && $toDay >= $day->everyDay()){
+        if ($startDay <= $day->everyDay() && $toDay >= $day->everyDay()) {
           // passed-dayを追加し過去の日付をグレーに
           $html[] = '<td class="calendar-td passed-day">';
-        }else{
-          $html[] = '<td class="calendar-td '.$day->getClassName().'">';
+        } else {
+          $html[] = '<td class="calendar-td ' . $day->getClassName() . '">';
         }
         $html[] = $day->render();
+        // 過去の日付に参加日を表示
+        if ($startDay <= $day->everyDay() && $toDay >= $day->everyDay()) {
+          // $reservePart = $day->authReserveDate($toDay >= $day->everyDay())->first()->setting_part;
+          // if ($reservePart == 1) {
+          //   $reservePart = "1部参加";
+          // } else if ($reservePart == 2) {
+          //   $reservePart = "2部参加";
+          // } else if ($reservePart == 3) {
+          //   $reservePart = "3部参加";
+          // } else {
+            // 受付終了
+            $html[] = '<p class="border-primary" style="width:70px; border-radius:5px;">受付終了</p>';
+          // }
+        }
 
-        if(in_array($day->everyDay(), $day->authReserveDay())){
+        // 今日以降で予約している日に予約部を表示する（はじめから記述されていた(頭にelse追加した)）
+        else if (in_array($day->everyDay(), $day->authReserveDay())) {
           $reservePart = $day->authReserveDate($day->everyDay())->first()->setting_part;
-          if($reservePart == 1){
+          if ($reservePart == 1) {
             $reservePart = "リモ1部";
-          }else if($reservePart == 2){
+          } else if ($reservePart == 2) {
             $reservePart = "リモ2部";
-          }else if($reservePart == 3){
+          } else if ($reservePart == 3) {
             $reservePart = "リモ3部";
           }
-          if($startDay <= $day->everyDay() && $toDay >= $day->everyDay()){
+          if ($startDay <= $day->everyDay() && $toDay >= $day->everyDay()) {
             $html[] = '<p class="m-auto p-0 w-75" style="font-size:12px"></p>';
             $html[] = '<input type="hidden" name="getPart[]" value="" form="reserveParts">';
-          }else{
-            $html[] = '<button type="submit" class="btn btn-danger p-0 w-75" name="delete_date" style="font-size:12px" value="'. $day->authReserveDate($day->everyDay())->first()->setting_reserve .'">'. $reservePart .'</button>';
+          } else {
+            $html[] = '<button type="submit" class="btn btn-danger p-0 w-75" name="delete_date" style="font-size:12px" value="' . $day->authReserveDate($day->everyDay())->first()->setting_reserve . '">' . $reservePart . '</button>';
             $html[] = '<input type="hidden" name="getPart[]" value="" form="reserveParts">';
           }
-        }else{
+        } else {
           $html[] = $day->selectPart($day->everyDay());
         }
         $html[] = $day->getDate();
@@ -75,20 +95,21 @@ class CalendarView{
     $html[] = '</tbody>';
     $html[] = '</table>';
     $html[] = '</div>';
-    $html[] = '<form action="/reserve/calendar" method="post" id="reserveParts">'.csrf_field().'</form>';
-    $html[] = '<form action="/delete/calendar" method="post" id="deleteParts">'.csrf_field().'</form>';
+    $html[] = '<form action="/reserve/calendar" method="post" id="reserveParts">' . csrf_field() . '</form>';
+    $html[] = '<form action="/delete/calendar" method="post" id="deleteParts">' . csrf_field() . '</form>';
 
     return implode('', $html);
   }
 
-  protected function getWeeks(){
+  protected function getWeeks()
+  {
     $weeks = [];
     $firstDay = $this->carbon->copy()->firstOfMonth();
     $lastDay = $this->carbon->copy()->lastOfMonth();
     $week = new CalendarWeek($firstDay->copy());
     $weeks[] = $week;
     $tmpDay = $firstDay->copy()->addDay(7)->startOfWeek();
-    while($tmpDay->lte($lastDay)){
+    while ($tmpDay->lte($lastDay)) {
       $week = new CalendarWeek($tmpDay, count($weeks));
       $weeks[] = $week;
       $tmpDay->addDay(7);
--- app/Calendars/General/CalendarWeekDay.php
+++ app/Calendars/General/CalendarWeekDay.php
@@ -1,22 +1,28 @@
 <?php
+
 namespace App\Calendars\General;
 
 use App\Models\Calendars\ReserveSettings;
+use App\Calendars\General\CalendarView;
 use Carbon\Carbon;
 use Auth;
 
-class CalendarWeekDay{
+class CalendarWeekDay
+{
   protected $carbon;
 
-  function __construct($date){
+  function __construct($date)
+  {
     $this->carbon = new Carbon($date);
   }
 
-  function getClassName(){
+  function getClassName()
+  {
     return "day-" . strtolower($this->carbon->format("D"));
   }
 
-  function pastClassName(){
+  function pastClassName()
+  {
     return;
   }
 
@@ -24,69 +30,74 @@ class CalendarWeekDay{
    * @return
    */
 
-   function render(){
-     return '<p class="day">' . $this->carbon->format("j"). '日</p>';
-   }
+  function render()
+  {
+    return '<p class="day">' . $this->carbon->format("j") . '日</p>';
+  }
 
-   function selectPart($ymd){
-     $one_part_frame = ReserveSettings::with('users')->where('setting_reserve', $ymd)->where('setting_part', '1')->first();
-     $two_part_frame = ReserveSettings::with('users')->where('setting_reserve', $ymd)->where('setting_part', '2')->first();
-     $three_part_frame = ReserveSettings::with('users')->where('setting_reserve', $ymd)->where('setting_part', '3')->first();
-     if($one_part_frame){
-       $one_part_frame = ReserveSettings::with('users')->where('setting_reserve', $ymd)->where('setting_part', '1')->first()->limit_users;
-     }else{
-       $one_part_frame = '0';
-     }
-     if($two_part_frame){
-       $two_part_frame = ReserveSettings::with('users')->where('setting_reserve', $ymd)->where('setting_part', '2')->first()->limit_users;
-     }else{
-       $two_part_frame = '0';
-     }
-     if($three_part_frame){
-       $three_part_frame = ReserveSettings::with('users')->where('setting_reserve', $ymd)->where('setting_part', '3')->first()->limit_users;
-     }else{
-       $three_part_frame = '0';
-     }
+  function selectPart($ymd)
+  {
+    $one_part_frame = ReserveSettings::with('users')->where('setting_reserve', $ymd)->where('setting_part', '1')->first();
+    $two_part_frame = ReserveSettings::with('users')->where('setting_reserve', $ymd)->where('setting_part', '2')->first();
+    $three_part_frame = ReserveSettings::with('users')->where('setting_reserve', $ymd)->where('setting_part', '3')->first();
+    if ($one_part_frame) {
+      $one_part_frame = ReserveSettings::with('users')->where('setting_reserve', $ymd)->where('setting_part', '1')->first()->limit_users;
+    } else {
+      $one_part_frame = '0';
+    }
+    if ($two_part_frame) {
+      $two_part_frame = ReserveSettings::with('users')->where('setting_reserve', $ymd)->where('setting_part', '2')->first()->limit_users;
+    } else {
+      $two_part_frame = '0';
+    }
+    if ($three_part_frame) {
+      $three_part_frame = ReserveSettings::with('users')->where('setting_reserve', $ymd)->where('setting_part', '3')->first()->limit_users;
+    } else {
+      $three_part_frame = '0';
+    }
 
-     $html = [];
-     $html[] = '<select name="getPart[]" class="border-primary" style="width:70px; border-radius:5px;" form="reserveParts">';
-     $html[] = '<option value="" selected></option>';
+    $html = [];
+      $html[] = '<select name="getPart[]" class="border-primary" style="width:70px; border-radius:5px;" form="reserveParts">';
+    $html[] = '<option value="" selected></option>';
     //  0を"1"に変更
-    if($one_part_frame == "1"){
+    if ($one_part_frame == "1") {
       $html[] = '<option value="1" disabled>リモ1部(残り0枠)</option>';
-    }else{
-      $html[] = '<option value="1">リモ1部(残り'.$one_part_frame.'枠)</option>';
+    } else {
+      $html[] = '<option value="1">リモ1部(残り' . $one_part_frame . '枠)</option>';
     }
     //  0を"2"に変更
-    if($two_part_frame == "2"){
+    if ($two_part_frame == "2") {
       $html[] = '<option value="2" disabled>リモ2部(残り0枠)</option>';
-    }else{
-      $html[] = '<option value="2">リモ2部(残り'.$two_part_frame.'枠)</option>';
+    } else {
+      $html[] = '<option value="2">リモ2部(残り' . $two_part_frame . '枠)</option>';
     }
     //  0を"3"に変更
-     if($three_part_frame == "3"){
-       $html[] = '<option value="3" disabled>リモ3部(残り0枠)</option>';
-     }else{
-       $html[] = '<option value="3">リモ3部(残り'.$three_part_frame.'枠)</option>';
-     }
-     $html[] = '</select>';
-     return implode('', $html);
-   }
+    if ($three_part_frame == "3") {
+      $html[] = '<option value="3" disabled>リモ3部(残り0枠)</option>';
+    } else {
+      $html[] = '<option value="3">リモ3部(残り' . $three_part_frame . '枠)</option>';
+    }
+    $html[] = '</「>';
+    return implode('', $html);
+  }
 
-   function getDate(){
-     return '<input type="hidden" value="'. $this->carbon->format('Y-m-d') .'" name="getData[]" form="reserveParts">';
-   }
+  function getDate()
+  {
+    return '<input type="hidden" value="' . $this->carbon->format('Y-m-d') . '" name="getData[]" form="reserveParts">';
+  }
 
-   function everyDay(){
-     return $this->carbon->format('Y-m-d');
-   }
+  function everyDay()
+  {
+    return $this->carbon->format('Y-m-d');
+  }
 
-   function authReserveDay(){
-     return Auth::user()->reserveSettings->pluck('setting_reserve')->toArray();
-   }
+  function authReserveDay()
+  {
+    return Auth::user()->reserveSettings->pluck('setting_reserve')->toArray();
+  }
 
-   function authReserveDate($reserveDate){
-     return Auth::user()->reserveSettings->where('setting_reserve', $reserveDate);
-   }
-
+  function authReserveDate($reserveDate)
+  {
+    return Auth::user()->reserveSettings->where('setting_reserve', $reserveDate);
+  }
 }
