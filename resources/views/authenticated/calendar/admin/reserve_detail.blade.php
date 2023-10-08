ed0bac33ac8a36daa39b670c6698337c1e5d818e
YuikoSawada <yuiko.sawada419@gmail.com>
2023-10-08 16:35:29 +0900

    スクール予約確認画面の各部数のリンクから詳細ページへ遷移させる

--- resources/views/authenticated/calendar/admin/reserve_detail.blade.php
+++ resources/views/authenticated/calendar/admin/reserve_detail.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="vh-100 d-flex" style="align-items:center; justify-content:center;">
   <div class="w-50 m-auto h-75">
-    <p><span>日</span><span class="ml-3">部</span></p>
+    <p><span>{{$date}}日</span><span class="ml-3">{{$part}}部</span></p>
     <div class="h-75 border">
       <table class="">
         <tr class="text-center">
